4a6d76d65ea096cbdb5c2b368c87a6ad32a500ad
Pawel Gedlek <pawgedlek@gmail.com>
2025-01-10 23:10:34 +0100

    Implement searchByCategory test in ProductServiceTest

--- src/test/java/com/pgedlek/ecommerce/service/ProductServiceImplTest.java
+++ src/test/java/com/pgedlek/ecommerce/service/ProductServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.pgedlek.ecommerce.service;
 
 import com.pgedlek.ecommerce.exception.ApiException;
+import com.pgedlek.ecommerce.exception.ResourceNotFoundException;
+import com.pgedlek.ecommerce.model.Category;
 import com.pgedlek.ecommerce.model.Product;
 import com.pgedlek.ecommerce.payload.ProductDTO;
 import com.pgedlek.ecommerce.payload.ProductResponse;
@@ -18,6 +20,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.pgedlek.ecommerce.config.AppConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,8 +33,11 @@ class ProductServiceImplTest {
     private static final Integer TEST_PAGE_SIZE = Integer.valueOf(PAGE_SIZE);
     private static final String TEST_SORT_BY = SORT_PRODUCTS_BY;
     private static final String TEST_SORT_ORDER = SORT_DIR;
-    private static final Product TEST_PRODUCT = new Product(1L, "Name", "default.png", "Description", 1, 1.0, 0.0, 1.0);
-    private static final ProductDTO TEST_PRODUCT_DTO = new ProductDTO(1L, "Name", "default.png", "Description", 1, 1.0, 0.0, 1.0);
+    private static final Long PRODUCT_ID = 1L;
+    private static final Product TEST_PRODUCT = new Product(PRODUCT_ID, "Name", "default.png", "Description", 1, 1.0, 0.0, 1.0);
+    private static final ProductDTO TEST_PRODUCT_DTO = new ProductDTO(PRODUCT_ID, "Name", "default.png", "Description", 1, 1.0, 0.0, 1.0);
+    private static final Long CATEGORY_ID = 10L;
+    private static final Category TEST_CATEGORY = new Category(CATEGORY_ID, "Category name", List.of(TEST_PRODUCT));
 
     @Mock
     private ProductRepository productRepositoryMock;
@@ -74,6 +80,56 @@ class ProductServiceImplTest {
                 .isExactlyInstanceOf(ApiException.class)
                 .hasMessage("No products created till now.");
         verify(productRepositoryMock).findAll(PageRequest.of(TEST_PAGE_NUMBER, TEST_PAGE_SIZE, Sort.by(TEST_SORT_BY).ascending()));
+        verifyNoMoreInteractions(categoryRepositoryMock, productRepositoryMock, modelMapperMock);
+    }
+
+    @Test
+    void testSearchByCategory_Success() {
+        // given
+        List<Product> productList = List.of(TEST_PRODUCT);
+        when(categoryRepositoryMock.findById(CATEGORY_ID)).thenReturn(Optional.of(TEST_CATEGORY));
+        when(productRepositoryMock.findByCategoryOrderByPriceAsc(TEST_CATEGORY, PageRequest.of(TEST_PAGE_NUMBER,
+                TEST_PAGE_SIZE, Sort.by(TEST_SORT_BY).ascending()))).thenReturn(new PageImpl<>(productList));
+        when(modelMapperMock.map(TEST_PRODUCT, ProductDTO.class)).thenReturn(TEST_PRODUCT_DTO);
+
+        // when
+        ProductResponse allProducts = productService.searchByCategory(CATEGORY_ID, TEST_PAGE_NUMBER, TEST_PAGE_SIZE, TEST_SORT_BY, TEST_SORT_ORDER);
+
+        // then
+        assertThat(allProducts.getContent()).hasSameElementsAs(List.of(TEST_PRODUCT_DTO));
+        verify(categoryRepositoryMock).findById(CATEGORY_ID);
+        verify(productRepositoryMock).findByCategoryOrderByPriceAsc(TEST_CATEGORY, PageRequest.of(TEST_PAGE_NUMBER, TEST_PAGE_SIZE, Sort.by(TEST_SORT_BY).ascending()));
+        verify(modelMapperMock).map(TEST_PRODUCT, ProductDTO.class);
+        verifyNoMoreInteractions(categoryRepositoryMock, productRepositoryMock, modelMapperMock);
+    }
+
+    @Test
+    void testSearchByCategory_ResourceNotFound() {
+        // given
+        when(categoryRepositoryMock.findById(CATEGORY_ID)).thenReturn(Optional.empty());
+
+        // when
+        Throwable caughtException = catchThrowable(() -> productService.searchByCategory(CATEGORY_ID, TEST_PAGE_NUMBER, TEST_PAGE_SIZE, TEST_SORT_BY, TEST_SORT_ORDER));
+
+        // then
+        assertThat(caughtException).isExactlyInstanceOf(ResourceNotFoundException.class)
+                .hasMessage("Category not found with categoryId " + CATEGORY_ID);
+        verifyNoMoreInteractions(categoryRepositoryMock, productRepositoryMock, modelMapperMock);
+    }
+
+    @Test
+    void testSearchByCategory_NoProductsFound() {
+        // given
+        when(categoryRepositoryMock.findById(CATEGORY_ID)).thenReturn(Optional.of(TEST_CATEGORY));
+        when(productRepositoryMock.findByCategoryOrderByPriceAsc(TEST_CATEGORY, PageRequest.of(TEST_PAGE_NUMBER,
+                TEST_PAGE_SIZE, Sort.by(TEST_SORT_BY).ascending()))).thenReturn(new PageImpl<>(List.of()));
+
+        // when
+        Throwable caughtException = catchThrowable(() -> productService.searchByCategory(CATEGORY_ID, TEST_PAGE_NUMBER, TEST_PAGE_SIZE, TEST_SORT_BY, TEST_SORT_ORDER));
+
+        // then
+        assertThat(caughtException).isExactlyInstanceOf(ApiException.class)
+                .hasMessage("No products found with categoryId " + CATEGORY_ID);
         verifyNoMoreInteractions(categoryRepositoryMock, productRepositoryMock, modelMapperMock);
     }
 }
